774827f4dac90bf7d2109c7841bc427fed03d552
Christian Theil Have <christiantheilhave@gmail.com>
2013-11-14 16:37:44 +0100

    working version that generates nice latex output

--- util.pl
+++ util.pl
@@ -1,7 +1,21 @@
 random_split_seq(Seq, Part1, Part2) :-
 	length(Seq,L),
-	random_uniform(0,L,F),!,
+	writeln(L),
+	random_uniform(0,L,F),
 	L1 is round(F),
 	L #= L1+L2,
 	length(Part2,L2),
 	append(Part1,Part2,Seq).
+
+random_split_seq_min(Seq, Part1, Part2,MinLen2) :-
+	length(Seq,L),
+	writeln(L),
+	LMax is L - MinLen2,
+	random_uniform(0,LMax,F),
+	L1 is round(F),
+	L #= L1+L2,
+	length(Part2,L2),
+	append(Part1,Part2,Seq).
+
+
+
